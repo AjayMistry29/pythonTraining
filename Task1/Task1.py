7a91b47b69aa3cd8bac8d58eabff1cf3da3b3fa3
ajay2929 <ajay.mistry92@gmail.com>
2021-02-01 20:22:19 -0500

    Task1 updated

---
+++ Task1/Task1.py
@@ -0,0 +1,77 @@
+# Q1 Define diff types of Variable
+# a,b,c = 1,2.5,"Task 1"
+# print(a,b,c)
+
+# Q2 Swap complex to int variable types and value
+# complexVar = 20 + 4j
+# print(type(complexVar))
+# intVar = 10
+
+# complexVar, intVar = intVar, complexVar
+
+# print ("Converted Complex:", complexVar)
+# print ("Converted Int:", intVar)
+
+# Q3 Swap two numbers using a third variable and do the same task without using any third variable.
+
+############## Part 1 ###############
+# x = 5
+# y = 7
+ 
+# print ("Before swapping: ")
+# print("Value of x : ", x, " and y : ", y)
+ 
+# x, y = y, x
+ 
+# print ("After swapping: ")
+# print("Value of x : ", x, " and y : ", y)
+
+# Q3 Swap two numbers using a third variable and do the same task without using any third variable.
+
+############## Part 2 ###############
+
+# x = 5
+# y = 10
+
+# temp = x
+# x = y
+# y = temp
+
+# print('The value of x after swapping: {}'.format(x))
+# print('The value of y after swapping: {}'.format(y))
+
+# Q4 Write a program that takes input from the user and prints it using both Python 2.x and Python 3.xVersion.
+
+# userInput = input( )
+# print ("Your entered text is: ", userInput)
+
+# Q5. Write a program to complete the task given below:
+#     Ask users to enter any 2 numbers in between 1-10 ,
+#     add the two numbers and keep the sum inanother variable called z.
+#     Add 30 to z and store the output in variable result and print result as thefinal output.
+
+# numFirst = int(input("Please enter any one number between 1 to 10 :" ))
+# numSecond = int(input("Please enter second number between 1 to 10 :" ))
+
+# z = numFirst + numSecond
+# result = z + 30
+# print("The final output:", result)
+
+# 6.Write a program to check the data type of the entered values.
+# HINT: Printed output should say -The data type of the input value is : int/float/string/etc
+
+# enterd_value = type(eval(input("Enter something: " )))
+
+# print(enterd_value)
+
+# 7.Create Variables using formats such as Upper CamelCase, Lower CamelCase, SnakeCase andUPPERCASE.
+
+# lower_camel = 23
+# Upper_Camel = 34
+# snake_case = 45
+# UPPER_CASE = 23
+
+# 8.If one data type value is assigned to ‘a’ variable and then a different data type value is assigned to ‘a’again.
+#   Will it change the value? If Yes then Why?
+# --> Yes, "a" will become a holder of latest value allocation. 
+
