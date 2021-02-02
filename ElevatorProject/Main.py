5ddef42954a65aad49d4e33bdc5204c65c3a3732
ajay2929 <ajay.mistry92@gmail.com>
2021-02-01 20:06:32 -0500

    Python Project-1 Completed

---
+++ ElevatorProject/Main.py
@@ -0,0 +1,85 @@
+import time
+
+print ("Welcome to ConsultAdd Elevator System! ")
+time.sleep(2)
+ 
+user_input = int(input("Press the floor number you wish to go:  "))
+
+if user_input == 1:
+    time.sleep(2)
+    print("Doors are opening...")
+    time.sleep(3)
+    print("Doors are closing...")
+    time.sleep(3)
+    print("Elevator is going up...")
+
+    time.sleep(4)
+    print("Doors are opening...")
+
+    time.sleep(2)
+    print("Elevator is reached to Floor one. You can step outside now. Have a nice day!")
+    
+if user_input == 2:
+    time.sleep(2)
+    print("Doors are opening...")
+    time.sleep(3)
+    print("Doors are closing...")
+    time.sleep(3)
+    print("Elevator is going up...")
+
+    time.sleep(6)
+    print("Doors are opening...")
+
+    time.sleep(2)
+    print("Elevator is reached to Floor two. You can step outside now. Have a nice day!")
+
+if user_input == 3:
+    time.sleep(2)
+    print("Doors are opening...")
+    time.sleep(3)
+    print("Doors are closing...")
+    time.sleep(3)
+    print("Elevator is going up...")
+
+    time.sleep(8)
+    print("Doors are opening...")
+
+    time.sleep(2)
+    print("Elevator is reached to Floor third. You can step outside now. Have a nice day!")
+
+if user_input == 4:
+    time.sleep(2)
+    print("Doors are opening...")
+    time.sleep(3)
+    print("Doors are closing...")
+    time.sleep(3)
+    print("Elevator is going up...")
+
+    time.sleep(10)
+    print("Doors are opening...")
+
+    time.sleep(2)
+    print("Elevator is reached to Floor forth. You can step outside now. Have a nice day!")
+
+if user_input == 5:
+    time.sleep(2)
+    print("Doors are opening...")
+    time.sleep(3)
+    print("Doors are closing...")
+    time.sleep(3)
+    print("Elevator is going up...")
+
+    time.sleep(12)
+    print("Doors are opening...")
+
+    time.sleep(2)
+    print("Elevator is reached to Floor fifth. You can step outside now. Have a nice day!")
+
+
+    
+
+
+
+
+    
+
